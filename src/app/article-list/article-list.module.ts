a11a08585d3a4f736474939cd8e8fe42d565fdf1
sergey <seryoga279@gmail.com>
2020-10-24 23:08:43 +0800

    feat: add header component

--- src/app/article-list/article-list.module.ts
+++ src/app/article-list/article-list.module.ts
@@ -4,9 +4,10 @@ import {ArticleListRoutingModule} from './article-list-routing.module';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleComponent } from './article/article.component';
 import { ArticleItemComponent } from './article-list/article-item/article-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [ArticleListComponent, ArticleComponent, ArticleItemComponent],
+  declarations: [ArticleListComponent, ArticleComponent, ArticleItemComponent, HeaderComponent],
   imports: [
     CommonModule,
     ArticleListRoutingModule
