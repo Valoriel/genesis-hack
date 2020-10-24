920692e3301c1936548f0af6dc2b2b1256594649
sergey <seryoga279@gmail.com>
2020-10-24 21:15:47 +0800

    feat: add article-list

--- src/app/article-list/article-list.module.ts
+++ src/app/article-list/article-list.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import {ArticleListRoutingModule} from './article-list-routing.module';
 import { ArticleListComponent } from './article-list/article-list.component';
 import { ArticleComponent } from './article/article.component';
+import { ArticleItemComponent } from './article-list/article-item/article-item.component';
 
 @NgModule({
-  declarations: [ArticleListComponent, ArticleComponent],
+  declarations: [ArticleListComponent, ArticleComponent, ArticleItemComponent],
   imports: [
     CommonModule,
     ArticleListRoutingModule
